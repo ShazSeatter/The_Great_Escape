eb32739b0181bd8a55739273ca3b24b28938eafb
ShazSeatter <shazseatter12@gmail.com>
2023-04-29 23:31:00 +0100

    final save for the day

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -59,6 +60,23 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    public bool _isFacingRight = true;
+    public bool IsFacingRight
+    {
+        get
+        {
+            return _isFacingRight;
+        }
+        private set
+        {
+            if(_isFacingRight != value)
+            {
+                // Flip the local scale to make the player face the opposite direction
+                transform.localScale *= new Vector2(-1, 1); 
+            }
+            _isFacingRight = value;
+        }
+    }
     Rigidbody2D rb;
 
     Animator animator;
@@ -95,6 +113,20 @@ public class PlayerController : MonoBehaviour
         moveInput = context.ReadValue<Vector2>();
 
         IsMoving = moveInput != Vector2.zero;
+
+        SetFacingDirection(moveInput);
+    }
+
+    private void SetFacingDirection(Vector2 moveInput)
+    {
+        if(moveInput.x > 0 && !IsFacingRight)
+        {
+            // Face the right
+            IsFacingRight = true;
+        } else if(moveInput.x < 0 && IsFacingRight) {
+            // Face the left
+            IsFacingRight = false;
+        }
     }
 
     public void OnRun(InputAction.CallbackContext context)
