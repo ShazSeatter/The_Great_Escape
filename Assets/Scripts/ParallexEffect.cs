ef78a10fb8989274920ecce527b3c442a93e6fe0
ShazSeatter <shazseatter12@gmail.com>
2023-04-29 22:14:33 +0100

    parallax effect added and applied to each layer
    parallax effect added to background and applied to each layer

---
+++ Assets/Scripts/ParallexEffect.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallexEffect : MonoBehaviour
+{
+    public Camera cam;
+    public Transform followTarget;
+
+    // Starting position for the parallax game object
+    Vector2 startingPosition;
+
+    // Start Z Value of the parallax game object
+    float startingZ;
+
+    // Distance that the camera has moved from the starting position of the parallax object
+    Vector2 camMoveSinceStart => (Vector2)cam.transform.transform.position - startingPosition;
+
+    float zDistanceFromTarget => transform.position.z - followTarget.transform.position.z;
+
+    float clippingPlane => (cam.transform.position.z + (zDistanceFromTarget > 0 ? cam.farClipPlane : cam.nearClipPlane));
+
+
+    // The further the object from the player, the faster the ParallaxEffect object will move. Drag it's Z value closer to the target to make it move slower.
+    float parallaxFactor => Mathf.Abs(zDistanceFromTarget) / clippingPlane;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPosition = transform.position;
+        startingZ = transform.position.z;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // when the target moves, move the parallax object the same distance times a multipler
+        Vector2 newPosition = startingPosition + camMoveSinceStart * parallaxFactor;
+
+        // the X/Y position changes based on target travel speed times the parallax factor, but Z stays consistent 
+        transform.position = new Vector3(newPosition.x, newPosition.y, startingZ);
+    }
+}
