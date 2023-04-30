eeadb8f8e429741ac3ae08c98023bd23a0c9d008
ShazSeatter <shazseatter12@gmail.com>
2023-04-30 22:15:59 +0100

    save for day

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,26 +6,45 @@ using UnityEngine.InputSystem;
 
 // the : means inherited from something in this case MonoBehaviour (like extends in java)
 
-[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Rigidbody2D), typeof(TouchingDirections))]
 public class PlayerController : MonoBehaviour
 {
     public float walkSpeed = 5f;
     public float runSpeed = 8f;
+    public float jumpImpulse = 6f;
+    public float airWalkSpeed = 3f;
+
+    //public float gravityScale = 1f;
+    //public float fallingGravityScale = 3f;
+
+
     Vector2 moveInput;
+    TouchingDirections touchingDirections;
+
 
 
     public float CurrentMoveSpeed { get
         {
-            if(IsMoving)
+            if (IsMoving && !touchingDirections.IsOnWall)
             {
-                if(IsRunning)
+                if (touchingDirections.IsGrounded)
                 {
-                    return runSpeed;
-                } else
-                {
-                    return walkSpeed;
+                    if (IsRunning)
+                    {
+                        return runSpeed;
+                    }
+                    else
+                    {
+                        return walkSpeed;
+                    }
                 }
-            } else
+                else
+                {
+                    // Air move
+                    return airWalkSpeed;
+                }
+            }
+            else
             {
                 // idle speed is 0
                 return 0;
@@ -77,6 +96,7 @@ public class PlayerController : MonoBehaviour
             _isFacingRight = value;
         }
     }
+
     Rigidbody2D rb;
 
     Animator animator;
@@ -85,6 +105,7 @@ public class PlayerController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        touchingDirections = GetComponent<TouchingDirections>();
     }
 
     // Start is called before the first frame update
@@ -96,13 +117,14 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void FixedUpdate()
     {
         // creating a new vector object and assigning it to rb.velocity. takes 2 inputs, which is x and y) 
         rb.velocity = new Vector2(moveInput.x * CurrentMoveSpeed, rb.velocity.y);
+        animator.SetFloat("yVelocity", rb.velocity.y);
     }
 
     // function that makes the character move set to true or false
@@ -139,4 +161,24 @@ public class PlayerController : MonoBehaviour
             IsRunning = false;
         }
     }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        // TODO check if alive as well
+        if(context.started && touchingDirections.IsGrounded)
+        {
+            animator.SetTrigger("jump");
+            rb.velocity = new Vector2(rb.velocity.x, jumpImpulse);
+        }
+       
+    }
+
+    public void OnAttack(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            animator.SetTrigger("attack");
+        }
+    }
 }
+
---
+++ Assets/Scripts/TouchingDirections.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Uses the collider to check directions to see if the object is currently on the ground, touching the wall, or touching the ceiling
+public class TouchingDirections : MonoBehaviour
+{
+    public ContactFilter2D castFilter;
+    public float groundDistance = 0.05f;
+    public float wallDistance = 0.2f;
+    public float ceilingDistance = 0.05f;
+
+    CapsuleCollider2D touchingCol;
+    Animator animator;
+
+    RaycastHit2D[] groundHits = new RaycastHit2D[5];
+    RaycastHit2D[] wallHits = new RaycastHit2D[5];
+    RaycastHit2D[] ceilingHits = new RaycastHit2D[5];
+
+    // Ground
+    [SerializeField]
+    private bool _isGrounded = true;
+
+    
+    public bool IsGrounded { get
+        {
+            return _isGrounded;
+        } private set
+        {
+            _isGrounded = value;
+            animator.SetBool("IsGrounded", value);
+        }
+            }
+
+    // Wall
+    [SerializeField]
+    private bool _isOnWall = true;
+
+
+    public bool IsOnWall
+    {
+        get
+        {
+            return _isOnWall;
+        }
+        private set
+        {
+            _isOnWall = value;
+            animator.SetBool("IsOnWall", value);
+        }
+    }
+
+
+    // Ceiling
+    [SerializeField]
+    private bool _isOnCeiling = true;
+    private Vector2 wallCheckDirection => gameObject.transform.localScale.x > 0 ? Vector2.right : Vector2.left;
+
+    public bool IsOnCeiling
+    {
+        get
+        {
+            return _isOnCeiling;
+        }
+        private set
+        {
+            _isOnCeiling = value;
+            animator.SetBool("IsOnCeiling", value);
+        }
+    }
+
+    private void Awake()
+    {
+        touchingCol = GetComponent<CapsuleCollider2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        IsGrounded = touchingCol.Cast(Vector2.down, castFilter, groundHits, groundDistance) > 0;
+        IsOnWall = touchingCol.Cast(wallCheckDirection, castFilter, wallHits, wallDistance) > 0;
+        IsOnCeiling = touchingCol.Cast(Vector2.up, castFilter, ceilingHits, ceilingDistance) > 0;
+    }
+
+}
