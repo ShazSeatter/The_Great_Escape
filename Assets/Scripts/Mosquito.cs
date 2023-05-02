cc24c37dcd4df8c0124d35850a35f1419994aef9
ShazSeatter <shazseatter12@gmail.com>
2023-05-02 16:17:03 +0100

    collider setttings changed for mosquito enemy

--- Assets/Scripts/Mosquito.cs
+++ Assets/Scripts/Mosquito.cs
@@ -3,16 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Mosquito : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+{   
+
+    public DetectionZone stabDetectionZone;
+
+    Animator animator;
+
+    Rigidbody2D rb;
+
+    // setting the inital value
+    public bool _hasTarget = false;
+
+    public bool HasTarget
     {
-        
+        get
+        {
+            return _hasTarget;
+        }
+        private set
+        {
+            _hasTarget = value;
+            animator.SetBool("hasTarget", value);
+        }
+    }
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // checking to see if there is any collideres detected if there is there is a target
+        HasTarget = stabDetectionZone.detectedColliders.Count > 0;
     }
 }
