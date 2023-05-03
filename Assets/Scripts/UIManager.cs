080de3f1c23a02d96b15ad7814127c1b6504f8f3
ShazSeatter <shazseatter12@gmail.com>
2023-05-03 10:07:52 +0100

    quit scene manager

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -77,10 +77,21 @@ public class UIManager : MonoBehaviour
 
     public void OnExitGame(InputAction.CallbackContext context)
     {
-        #if (context.started)
+        if (context.started)
         {
+            #if (UNITY_EDITOR || DEVELOPMENT_BUILD)
 
+                        Debug.Log(this.name + " : " + this.GetType() + " : " + System.Reflection.MethodBase.GetCurrentMethod().Name);
+
+            #endif
+
+            #if (UNITY_EDITOR)
+                        UnityEditor.EditorApplication.isPlaying = false;
+            #elif (UNITY_STANDALONE)
+                        Application.Quit();
+            #elif (UNITY_WEBGL)
+                        SceneManager.LoadScene("QuitScene");
+            #endif
         }
-        #endif
+        }
     }
-}
