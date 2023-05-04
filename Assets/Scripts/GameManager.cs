7a63e379dfa99e70c282c1b91b719a8a794b173c
ShazSeatter <shazseatter12@gmail.com>
2023-05-04 16:30:28 +0100

    main menu test scene added

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -30,4 +30,9 @@ public class GameManager : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
+
+    public void mainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
 }
